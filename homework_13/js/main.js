624d5701e399d83ac11f33712f594042505aef44
LamberJack <ivzet009@gmail.com>
2025-09-25 21:39:41 +0300

    add program

--- homework_13/js/main.js
+++ homework_13/js/main.js
@@ -1,7 +1,7 @@
 
 (function () {
+  // Бургер меню
   document.addEventListener('click', burgerInin)
-
   function burgerInin(e) {
     const target = e.target
     const burgerIcon = target.closest(".burger-icon")
@@ -14,10 +14,11 @@
   }
   const about__img = document.querySelector(".about__img-button")
   const modal = document.querySelector(".modal")
+  // --------------------------------------------------
 
+  // Модалка 
   about__img.addEventListener('click', modaOpen)
   modal.addEventListener("click", modalClose)
-
   function modaOpen(e) {
     e.preventDefault()
     document.body.classList.add("body--opened-modal")
@@ -31,4 +32,54 @@
     }
 
   }
+  // ---------------------------------------------------
+
+  // Таб
+  const tabControls = document.querySelector(".tab-controls")
+
+  tabControls.addEventListener("click", toggleTab)
+
+  function toggleTab(e) {
+    const target = e.target
+    const tabControl = target.closest(".tab-controls__link")
+
+    if (!tabControl) return
+    e.preventDefault()
+    if (tabControl.classList.contains("tab-controls--active")) return
+
+
+    const tabContentID = tabControl.getAttribute("href")
+    const tabContent = document.querySelector(tabContentID)
+    const tabContentShow = document.querySelector(".tab-content--show")
+    const tabControlsActive = document.querySelector(".tab-controls--active")
+
+    tabContentShow.classList.remove("tab-content--show")
+    tabControlsActive.classList.remove("tab-controls--active")
+
+    tabContent.classList.add("tab-content--show")
+    tabControl.classList.add('tab-controls--active')
+  }
+  //------------------------------------------------------
+
+
+  //  Акардион 
+
+  const accordionLists = document.querySelectorAll(".accordion-list")
+  accordionLists.forEach(el => {
+    el.addEventListener("click", e => {
+      const accordionList = e.currentTarget
+      const accordionOpen = accordionList.querySelector(".accordion-list__item--opened")
+      const accordionControl = e.target.closest(".accordion-list__control")
+
+      if (!accordionControl) return
+      const accordionItem = accordionControl.parentElement;
+      if (accordionOpen && accordionItem != accordionOpen) {
+        accordionOpen.classList.remove("accordion-list__item--opened")
+      }
+
+      accordionItem.classList.toggle("accordion-list__item--opened")
+
+
+    })
+  })
 })()
